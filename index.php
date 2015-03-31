00307ee42a98f4ded8f1663b48001fc0e708e962
rubalc09 <rubalc.castro@hotmail.com>
2015-03-30 23:57:28 -0500

    2

--- index.php
+++ index.php
@@ -1,110 +1,92 @@
-<?php
-require_once("class/class.php");
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-
-
-
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<meta charset="utf-8" />
-<link rel="shortcut icon" href="public/images/favicon.ico">
-<title>..:: Intranet  - Usuarios ::..</title>
-<!-- MASTER STYLESHEET-->
-<link href="public/css/styles.css" rel="stylesheet" type="text/css" />
-
-<!-- LOAD JQUERY -->
-<script type="text/javascript" src="public/js/jquery.js" language="javascript"></script>
-
-
-<script language="javascript" type="text/javascript" src="public/js/funciones.js"></script>   
-
-
-<!-- LOAD POPUP -->
-<script type="text/javascript" src="public/js/jquery-1.7.1.min.js" language="javascript"></script>
-<script type="text/javascript" src="public/js/jquery.fancybox.pack.js" language="javascript"></script>
-<script type="text/javascript" src="public/js/poput.js" language="javascript"></script>
-<script type="text/javascript" src="public/js/emergente.js" language="javascript"></script>
-
-<link rel="stylesheet" href="public/js/jquery.fancybox.css" language="javascript"></script>
-
-<!-- LOAD POPUP -->
-
-<!-- CUSTOM JQUERY AND FEATURES LOADER-->
-<script src="public/js/scripts.js" type="text/javascript"></script>
-
-<!-- LOAD TABLESORTER -->
-<script type="text/javascript" src="public/js/jquery.tablesorter.min.js"></script>
-<script type="text/javascript" src="public/js/jquery.tablesorter.pager.js"></script>
-
-<!-- LOAD WYSIWYG EDITOR -->
-<script type="text/javascript" src="public/js/jquery.wysiwyg.js"></script>
-<link href="public/css/jquery.wysiwyg.css" rel="stylesheet" type="text/css" />
-
-<!-- LOAD FACEBOX -->
-<script type="text/javascript" src="public/js/facebox.js"></script>
-<link href="public/css/facebox.css" rel="stylesheet" type="text/css" />
-<link href="public/css/menu.css" rel="stylesheet" type="text/css" />
-
-<title>PHP Data Objects</title>
-</head>
-
-<body>
-
-<hr />
-<?php
-
-
-$obj= new Trabajo();
-$datos=$obj->get_preguntas();
-
-
-?>
-
-
-<div id="content_wrapper">
-
-      <div id="primary_content">
-
-   <form action="resultado.php" method="post">
-        <?php
-         for($i=0;$i<sizeof($datos);$i++)
-        {   ?>
-     <div id="nombre">
-      <?php
-          echo $datos[$i]["pregunta"];
-         ?>
-            
- </div>
- 
-              <div id="accionboton">  
-          <input type = "radio"
-                 name = "pregunta_<?php echo $datos[$i]["idpregunta"];?>"
-                
-                 value = "si"
-                 checked = "checked" />si
-          
-          <input type = "radio"
-                 name = "pregunta_<?php echo $datos[$i]["idpregunta"];?>"
-            
-                 value = "no" />no
- 
-           </div>
-
+<!DOCTYPE html>
+<html>
+	<head>
+        <title>Predial-Paracas</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <!--[if lt IE 9]>
+        <script type="text/javascript">
+           document.createElement("nav");
+           document.createElement("header");
+           document.createElement("footer");
+           document.createElement("section");
+           document.createElement("article");
+           document.createElement("aside");
+           document.createElement("hgroup");
+        </script>
+        <![endif]-->
+	</head>
+	<body>
         
-      <hr>
-         
-            <?php
-        }
-        ?>
-<input type="submit" value="enviar">
-
-</form>
-</div>
-</div>
-
-
-</body>
+        <div class="wrap">
+            <header>
+                 <h1>"MUNICIPALIDAD DISTRITAL DE PARACAS"</h1>  
+                 <H2>AÑO 2014</H2>
+            </header>
+            <section class='menu'>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href='./Administracion/usuarios.php' title='Notas'>
+                                <div class='icono'>
+                                    <img src='img/icoPartes.png' alt=''>
+                                </div>
+                                <h2>Contribuyentes</h2>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='./Administracion/agua.php' title='Partes de trabajo'>
+                                <div class='icono'>
+                                    <img src='img/graphics.png' alt=''>
+                                </div>
+                                <h2>Agua</h2>
+                            </a>
+                        </li>  
+                        <li>
+                            <a href='./Administracion/arbitrios.php' title='Acciones Comerciales'>
+                                <div class='icono'>
+                                    <img src='img/page3-img5.png' alt=''>
+                                </div>
+                                <h2>Arbitrios</h2>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='./Administracion/im_predial.php' title='Incidencias'>
+                                <div class='icono'>
+                                    <img src='img/network_server.png' alt=''>
+                                </div>
+                                <h2>Impuesto Predial</h2>
+                            </a>
+                        </li>
+                         <li>
+                            <a href='./Administracion/reportes.php' title='Incidencias'>
+                                <div class='icono'>
+                                    <img src='img/page3-img6.png' alt=''>
+                                </div>
+                                <h2>Reportes</h2>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='./envio.php' title='calculo del impuesto predial'>
+                                <div class='icono'>
+                                    <img src='img/Man_Grey.png' alt=''>
+                                </div>
+                                <h2>Calculo Predial</h2>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+            <aside>
+                <div class="alert alert-success">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    Seleccione su opción pulsando sobre los botones.
+                </div>
+            </aside>    
+        </div>
+	</body>
 </html>
